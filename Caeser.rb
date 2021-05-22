f38ff3da8ab3ef9bcac6dcaf75162beac325fb22
technofoible <techno.foible@gmail.com>
2021-05-22 15:20:58 -0500

    added previously finished caeser cipher project to repo

---
+++ Caeser.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+# Caeser Cipher
+# Encrypts a message by shifting all letters right by given shift factor
+
+def caeser_cipher(message, shift)
+  char_list = message.chars
+  char_list.map! do |char|
+    char = char.ord
+    
+    if char.between?(65, 90)
+      char = (((char - 65) + shift) % 26) + 65
+    elsif char.between?(97, 122)
+      char = (((char - 97) + shift) % 26) + 97
+    else
+      char
+    end
+
+    char = char.chr
+  end
+  puts char_list.join("")
+end
+
+
+caeser_cipher("This is a test.", 5)
+caeser_cipher("My very secret message.", 3)
